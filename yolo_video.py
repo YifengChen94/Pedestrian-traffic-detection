47ef3aa967f8de93bb3b8a7a6941cda0e8b3981a
YifengChen94 <yfchen@stu.suda.edu.cn>
2018-09-05 19:08:38 +0800

    third commit

---
+++ yolo_video.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: $ python {0} [video_path] [output_path(optional)]", sys.argv[0])
+    exit()
+
+from yolo import YOLO
+from yolo import detect_video
+
+if __name__ == '__main__':
+    #detect_video(YOLO(),"railway.avi")
+    video_path = sys.argv[1]
+    if len(sys.argv) > 2:
+        output_path = sys.argv[2]
+        detect_video(YOLO(), video_path, output_path)
+    else:
+        detect_video(YOLO(), video_path)
